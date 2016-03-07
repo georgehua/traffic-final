1c2c88788c7efe6ade40382071f4cb4955e142e6
Joycie Yu <Joycie@Joycies-Wench.local>
2016-03-06 17:38:46 -0800

    editing back up

--- server.R
+++ server.R
@@ -8,18 +8,28 @@ A_520_70 <- read.csv("After SR 520 to NE 70.csv")
 #A_I5_527 <- read.csv("After I-5 to SR 527.csv")
 A_I5_522 <- read.csv("After I-5 to 522.csv")
 B_I5_522 <- read.csv("Before 1-5 to 522.csv")
+A_8_527 <- read.csv("After 8th to SR 527.csv")
+B_8_527 <- read.csv("Before 8th to SR 527.csv")
 
 shinyServer(function(input, output) {
-  output$plot <- renderPlotly({
+  output$plotBefore <- renderPlotly({
     if (input$select == 1) {
       data1 = B_520_70
-      data2 = A_520_70
-    } else {
+    } else if (input$select == 2) {
       data1 = B_I5_522
-      data2 = A_I5_522
+    } else {
+      data1 = B_8_527
     }
-    plot_ly(data = data1, x = Time, y = Avg..TTS, mode = "markers")
-    #plot_ly(data = data2, x = Time, y = Avg..TTS , mode = "markers")
-  
+    plot_ly(data = data1, x = Time, y = Avg..TTS / 60, mode = "markers")
+  })
+  output$plotAfter <- renderPlotly({
+    if (input$select == 1) {
+      data2 = A_520_70
+    } else if (input$select == 2) {
+      data2 = A_I5_522
+    } else {
+      data2 = A_8_527
+    }
+    plot_ly(data = data2, x = Time, y = Avg..TTS / 60, mode = "markers")
   })
 })
--- ui.R
+++ ui.R
@@ -11,17 +11,19 @@ A_520_70 <- read.csv("After SR 520 to NE 70.csv")
 #A_I5_527 <- read.csv("After I-5 to SR 527.csv")
 A_I5_522 <- read.csv("After I-5 to 522.csv")
 B_I5_522 <- read.csv("Before 1-5 to 522.csv")
-
+A_8_527 <- read.csv("After 8th to SR 527.csv")
+B_8_527 <- read.csv("Before 8th to SR 527.csv")
 
 shinyUI(fluidPage(
   titlePanel("Traffic Sux"),
   sidebarPanel(
     selectInput("select", label = h3("Select Corridor"), 
-              choices = list("520 to 70" = 1, "I-5 to 522" = 2), 
+              choices = list("520 to 70" = 1, "I-5 to 522" = 2, "8th to 527" == 3), 
               selected = 1)
   ),
   mainPanel(
-    plotlyOutput("plot")
+    plotlyOutput("plotBefore"),
+    plotlyOutput("plotAfter")
   )
   
   
