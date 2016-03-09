473047214a5f1cb17bc29719b4bc82d0babd57e5
Joycie Yu <Joycie@D-173-250-157-9.dhcp4.washington.edu>
2016-03-09 13:05:46 -0800

    added more data

--- server.R
+++ server.R
@@ -10,26 +10,53 @@ A_I5_522 <- read.csv("After I-5 to 522.csv")
 B_I5_522 <- read.csv("Before 1-5 to 522.csv")
 A_8_527 <- read.csv("After 8th to SR 527.csv")
 B_8_527 <- read.csv("Before 8th to SR 527.csv")
+A_bv_tl <- read.csv("After Bellevue to Totem Lk.csv")
+B_bv_tl <- read.csv("Before Bell to Ttm Lk.csv")
+A_85_520 <- read.csv("After NE85th to 520.csv")
+B_85_520 <- read.csv("Before NE85th to 520.csv")
 
 shinyServer(function(input, output) {
   output$plotBefore <- renderPlotly({
+    x <- list(
+      title = "Time"
+    )
+    y <- list(
+      title = "Before Avg Commute (min)"
+    )
     if (input$select == 1) {
       data1 = B_520_70
     } else if (input$select == 2) {
       data1 = B_I5_522
-    } else {
+    } else if (input$select == 3){
       data1 = B_8_527
+    } else if (input$select == 4){
+      data1 = B_bv_tl
+    } else {
+      data1 = B_85_520
     }
-    plot_ly(data = data1, x = Time, y = Avg..TTS / 60, mode = "markers")
+    plot_ly(data = data1, x = Time, y = X90..ile / 60, mode = "markers") %>% 
+      layout(xaxis=x, yaxis=y)
+    
   })
   output$plotAfter <- renderPlotly({
+    x <- list(
+      title = "Time"
+    )
+    y <- list(
+      title = "After Avg Commute (min)"
+    )
     if (input$select == 1) {
       data2 = A_520_70
     } else if (input$select == 2) {
       data2 = A_I5_522
-    } else {
+    } else if (input$select == 3) {
       data2 = A_8_527
+    } else if (input$select == 4) {
+      data2 = A_bv_tl
+    } else {
+      data2 = A_85_520
     }
-    plot_ly(data = data2, x = Time, y = Avg..TTS / 60, mode = "markers")
+    plot_ly(data = data2, x = Time, y = X90..ile / 60, mode = "markers") %>% 
+      layout(xaxis=x, yaxis=y)
   })
 })
--- ui.R
+++ ui.R
@@ -13,22 +13,21 @@ A_I5_522 <- read.csv("After I-5 to 522.csv")
 B_I5_522 <- read.csv("Before 1-5 to 522.csv")
 A_8_527 <- read.csv("After 8th to SR 527.csv")
 B_8_527 <- read.csv("Before 8th to SR 527.csv")
+A_bv_tl <- read.csv("After Bellevue to Totem Lk.csv")
+B_bv_tl <- read.csv("Before Bell to Ttm Lk.csv")
+A_85_520 <- read.csv("After NE85th to 520.csv")
+B_85_520 <- read.csv("Before NE85th to 520.csv")
 
 shinyUI(fluidPage(
   titlePanel("Traffic Sux"),
   sidebarPanel(
-    selectInput("select", label = h3("Select Corridor"), 
-              choices = list("520 to 70" = 1, "I-5 to 522" = 2, "8th to 527" == 3), 
-              selected = 1)
+     selectInput("select", label = h3("Select Corridor"), 
+               choices = list("520 to 70" = 1, "I-5 to 522" = 2, "8th to 527" = 3, "Bellevue to Totem Lk" = 4, "NE 85th to 520" = 5), 
+               selected = 1)
   ),
   mainPanel(
     plotlyOutput("plotBefore"),
     plotlyOutput("plotAfter")
   )
-  
-  
-  
-  
-  
-  
+
 ))
