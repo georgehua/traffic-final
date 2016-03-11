e0255f8f45961ede513547b700e6f01f5a1c78b6
estebanparreno <eparreno@uw.edu>
2016-03-10 22:25:48 -0800

    ui fix

--- ui.R
+++ ui.R
@@ -42,15 +42,11 @@ ui <- fluidPage( theme = "style.css",
                  
                  fluidRow(
 
-<<<<<<< HEAD
+
                    column(12,   tags$div(id="sliderhide",sliderInput("slider", label = h6("Select Time or Press Play"), min = 0.5, 
                               max = 24, step=0.5, animate= animationOptions(loop = TRUE, interval = 300), value = 0, width = "100%")
                  )),
-=======
-                   column(12,   sliderInput("slider", label = h3("Slider"), min = 1, 
-                              max = 288, step=1, animate= animationOptions(loop = TRUE, interval = 100), value = 0, width = "100%")
-                 ),
->>>>>>> a1211f5a41a1a272cb3525961931400cadc02a22
+
                  fluidRow(
                    column(12,
                           plotlyOutput("plotBefore"))
